5f117d589809cd912f945cfa30bd2a77cf53121a
HzDSN <HzDSN@DESKTOP-KHUSPCG>
2016-11-16 00:08:37 +0800

    添加项目文件。

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Form1_Load
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.textBox1.Text == "dsning01@gmail.com" && this.textBox2.Text == "55375b4cd4041119fa49d75013c9151f")
+            {
+                MessageBox.Show("Login success, redirecting...", "Login", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Invaild username or password, please try again.", "Login", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "dsning01@gmail.com";
+            this.textBox2.Text = "55375b4cd4041119fa49d75013c9151f";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            this.textBox2.Text = "";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            this.textBox2.Text = "DO NOT touch me!";
+            
+        }
+    }
+}
